48ae735e3bb4bc05bc5111066a27e46f45150424
jasandine <jasandine@gmail.com>
2015-02-05 09:40:12 -0800

    Add a feature that capitalizes the first word of a band name

--- lib/band.rb
+++ lib/band.rb
@@ -1,7 +1,14 @@
 class Band < ActiveRecord::Base
 
   has_and_belongs_to_many(:venues)
+  before_save(:capitalize_name)
 
 
 
+  private
+
+
+  define_method(:capitalize_name) do 
+    self.name = name.capitalize
+  end
 end
